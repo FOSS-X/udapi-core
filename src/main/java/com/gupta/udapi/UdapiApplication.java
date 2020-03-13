eb79d24f7c3c8992ba81e8e6ecb125d7c7eae300
Amit Kumar Gupta <amit.kumar.g@protonmail.ch>
2020-03-13 21:19:54 +0530

    Initial commit

---
+++ src/main/java/com/gupta/udapi/UdapiApplication.java
@@ -0,0 +1,15 @@
+package com.gupta.udapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdapiApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(UdapiApplication.class, args);
+	}
+
+}
